7ae31fcd868b3886d9a0329ab81863b18b9813cf
Cysmi <cnb10100@gmail.com>
2022-02-13 19:57:28 -0500

    restructured schema and created functions for index.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,28 +2,22 @@ DROP DATABASE IF EXISTS tracker_db;
 CREATE DATABASE tracker_db;
 USE tracker_db; 
 
+CREATE TABLE employee (
+ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INTEGER,
+manager_id INTEGER,
+);
+
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(30),
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL NOT NULL,
-    department_id INTEGER, 
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-);
-
-CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER, 
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary INTEGER (8,2),
+department_id INTEGER,
 );
--- server.js
+++
@@ -1,8 +0,0 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql2");
-const connection = require("./config/connection");
-
-
-
-
-
