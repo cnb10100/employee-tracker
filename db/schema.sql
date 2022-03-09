00a0025a13142a4490617a8276427724e61184bd
Cysmi <cnb10100@gmail.com>
2022-03-09 01:17:16 -0500

    modified schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,17 +7,17 @@ ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INTEGER,
-manager_id INTEGER,
+manager_id INTEGER
 );
 
 CREATE TABLE department (
 ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR(30),
+name VARCHAR(30)
 );
 
 CREATE TABLE role (
 ID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
-salary INTEGER (8,2),
-department_id INTEGER,
+salary DECIMAL (8,2),
+department_id INTEGER
 );
